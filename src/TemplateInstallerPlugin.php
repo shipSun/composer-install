98de5dc0ed90dbdea72c8ec8d5859a664bdf2bfd
shipSun <543999860@qq.com>
2018-06-08 19:15:32 +0800

    up

--- src/TemplateInstallerPlugin.php
+++ src/TemplateInstallerPlugin.php
@@ -4,6 +4,7 @@ namespace phpDocumentor\Composer;
 use Composer\Composer;
 use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
+use phpDocumentor\Composer\TemplateInstaller;
 
 class TemplateInstallerPlugin implements PluginInterface
 {
